b0fe39e0f75e5af027f94a97a98e1c31f7b31243
ifv-7-9 <ifernandezv2501@educantabria.es>
2026-03-08 14:52:05 +0100

    cdxfgdfgdf

---
+++ PHPregistro/registro.php
@@ -0,0 +1,17 @@
+<?php
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+echo "<h2>Registro completado</h2>";
+echo "Nombre: " . $nombre . "<br>";
+echo "Email: " . $email . "<br>";
+
+echo "<p>Ahora puedes acceder a descuentos en algunos productos.</p>";
+
+<li class="user-name">
+  Hola, <?php echo $nombre; ?>
+</li>
+
+?>
